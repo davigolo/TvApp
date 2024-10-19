141cb300f729dc7deada65b29ab259b452108ed4
davigolo <davidglom@ext.inditex.com>
2024-10-19 13:25:30 +0200

    Project

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TvApp"
+<<<<<<< Updated upstream
+include(":app")
+ 
+=======
+include(
+    ":app",
+    ":modules:designsystem",
+    ":modules:feature:home",
+    ":core:model",
+    ":core:feature:home:data",
+    ":core:feature:home:domain",
+)
+>>>>>>> Stashed changes
